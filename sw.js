f5454e8c5e27038b2f2aeef5b45d14504c719634
stevekariithi-letsbuild <steve.kariithi@dignitasproject.org>
2026-05-23 16:55:27 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'uniform-tracker-v3';
+const CACHE = 'uniform-tracker-v4';
 const ASSETS = ['/', '/index.html', '/app.js', '/manifest.json'];
 
 self.addEventListener('install', e => {
