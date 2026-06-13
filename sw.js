9c8b82f5d2c8c419aaee83a0341402d75a3966a9
stevekariithi-letsbuild <steve.kariithi@dignitasproject.org>
2026-06-13 15:17:50 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,23 @@
-const CACHE = 'uniform-v1';
-const ASSETS = ['/', '/index.html', '/app.js', '/manifest.json'];
+const CACHE = 'uniform-v2';
+const ASSETS = ['/index.html', '/app.js', '/manifest.json'];
 
-self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
-self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
