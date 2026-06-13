6a96340bb666dd6dad7587059e28d06ec99910ed
stevekariithi-letsbuild <steve.kariithi@dignitasproject.org>
2026-06-13 15:12:02 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,5 @@
-self.addEventListener('fetch', e => {});
+const CACHE = 'uniform-v1';
+const ASSETS = ['/', '/index.html', '/app.js', '/manifest.json'];
+
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
+self.addEventListener('fetch', e => e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))));
